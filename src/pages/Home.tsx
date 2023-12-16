596ee2ba73dc08dc8f46ce9a2cc5ea08b6ded5b6
Pavel <malinatrashh@gmail.com>
2023-12-16 10:20:56 +0800

    импортим компоненты по доке shadcn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,33 @@
+import { ThemeProvider } from '@/components/theme-provider'
+import { Button } from '@/components/ui/button'
+import { ToastAction } from '@/components/ui/toast'
+import { Toaster } from '@/components/ui/toaster'
+import { useToast } from '@/components/ui/use-toast'
+
 const Home = () => {
 	const wrapper = 'flex h-screen justify-center items-center select-none'
-	return <div className={wrapper}>Это домашняя страница</div>
+	const { toast } = useToast()
+	return (
+		<ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+			<div className={wrapper}>
+				<Button
+					variant='outline'
+					onClick={() => {
+						toast({
+							title: 'Scheduled: Catch up ',
+							description: 'Friday, February 10, 2023 at 5:57 PM',
+							action: (
+								<ToastAction altText='Goto schedule to undo'>Undo</ToastAction>
+							),
+						})
+					}}
+				>
+					Это домашняя страница
+				</Button>
+			</div>
+			<Toaster />
+		</ThemeProvider>
+	)
 }
 
 export default Home
