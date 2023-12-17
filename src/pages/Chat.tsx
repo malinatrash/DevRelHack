2be3b0abf2415aadd38463aa6a44f1ac9bf4e609
Pavel <malinatrashh@gmail.com>
2023-12-17 13:46:33 +0800

    Update Chat.tsx

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,26 +1,26 @@
 import SideBar from '@/components/SideBar'
+import { ChatExplorer } from '@/components/chat/ChatExplorer'
+import { ChatHeadear } from '@/components/chat/ChatHeadear'
+import ChatMessagesWindow from '@/components/chat/ChatMessagesWindow'
 import { ThemeProvider } from '@/components/theme-provider'
-import {ChatExplorer} from "../../../DevRelHack2/src/components/chat/ChatExplorer";
-import ChatMessagesWindow from "../../../DevRelHack2/src/components/chat/ChatMessagesWindow";
-import {ChatHeadear} from "../../../DevRelHack2/src/components/chat/ChatHeadear";
 
 const Chat = () => {
-    return (
-        <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-            <div className='flex'>
-                <SideBar />
-                <div className='flex-1 flex flex-col'>
-                    <div className='container mx-auto shadow-lg rounded-lg'>
-                        <ChatHeadear />
-                        <div className='flex'>
-                            <ChatExplorer />
-                            <ChatMessagesWindow />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </ThemeProvider>
-    )
+	return (
+		<ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+			<div className='flex'>
+				<SideBar />
+				<div className='flex-1 flex flex-col'>
+					<div className='container mx-auto shadow-lg rounded-lg'>
+						<ChatHeadear />
+						<div className='flex'>
+							<ChatExplorer />
+							<ChatMessagesWindow />
+						</div>
+					</div>
+				</div>
+			</div>
+		</ThemeProvider>
+	)
 }
 
 export default Chat
