fedb63939db3f11ddfa54d0352a8fedaeaee7033
Pavel <malinatrashh@gmail.com>
2023-12-17 13:36:40 +0800

    Update ProfileDashboard.tsx

--- src/components/profile/ProfileDashboard.tsx
+++ src/components/profile/ProfileDashboard.tsx
@@ -1,28 +1,38 @@
 import { useAppSelector } from '@/hooks/redux'
-import { devevents } from '@/models/user'
+import { User, devevents } from '@/models/user'
 import { parseDate } from '@/scripts/dates'
-import { FC } from 'react'
+import { fetchUsers } from '@/store/api/fetchUsers'
+import { FC, useEffect, useState } from 'react'
 import EventCardItem from '../home/EventCardItem'
+import UserCardItem from '../home/UserCardItem'
 import { Badge } from '../ui/badge'
 import { ScrollArea, ScrollBar } from '../ui/scroll-area'
 
 const ProfileDashboard: FC = () => {
 	const user = useAppSelector(state => state.userData)
-
+	const [users, setusers] = useState<User[]>([])
+	const fetchUser = async () => {
+		await fetchUsers().then(r => setusers(r))
+	}
+	useEffect(() => {
+		fetchUser()
+	}, [])
 	return (
-		<div className='pl-28 pt-8 min-h-screen max-h-full flex flex-col gap-12 px-8'>
+		<div className='pl-28 pt-8 min-h-screen max-h-full flex flex-col gap-12 px-8 w-[75vw]'>
 			<div>
-				<h1>Ваш Профиль {user.first_name}</h1>
+				<h1>Профиль</h1>
 			</div>
 			<div>
-				<h3>Роль: {user.is_staff ? 'Администратор' : 'Пользователь'}</h3>
+				<h3>Роль: {user.is_staff ? 'DevRel' : 'Пользователь'}</h3>
 				<h3>Дата Регистрации: {parseDate(user.date_joined ?? '')} </h3>
-				<div className='flex gap-3 w-full bg-secondary pr-32 p-8 rounded-lg mt-20'>
-					Ваши Навыки:{' '}
-					{user.specs?.map(e => (
-						<Badge children={e} />
-					))}
-				</div>
+				{!user.is_staff && (
+					<div className='flex gap-3 w-full bg-secondary pr-32 p-8 rounded-lg mt-20'>
+						Ваши Навыки:{' '}
+						{user.specs?.map(e => (
+							<Badge children={e} />
+						))}
+					</div>
+				)}
 			</div>
 			<div>
 				<h3>Недавние мероприятия</h3>
@@ -30,6 +40,17 @@ const ProfileDashboard: FC = () => {
 					<div className='flex space-x-4 p-4 max-w-[73vw]'>
 						{devevents.map(e => (
 							<EventCardItem event={e} />
+						))}
+					</div>
+					<ScrollBar orientation='horizontal' />
+				</ScrollArea>
+			</div>
+			<div className='w-auto'>
+				<h3>Вы недавно общались</h3>
+				<ScrollArea className=' rounded-md bg-secondary'>
+					<div className='flex space-x-4 p-4 max-w-[73vw]'>
+						{users.map(e => (
+							<UserCardItem key={e.id} user={e} />
 						))}
 					</div>
 					<ScrollBar orientation='horizontal' />
